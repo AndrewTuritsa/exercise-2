61adbe52ab76c72cab8431fa795bd119e834b162
Andrew <andrewturitsa@gmail.com>
2024-01-11 18:00:08 -0800

    code from class 2

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+import Link from 'next/link'
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+            Made by Andrew
+        </footer>
+    )
+
+}
